4aad3b7e1d996b118d71d37753ad828541abcab2
abudarda12 <abudardaansari66@gmail.com>
2025-05-10 11:52:30 +0530

    fixed margin and padding

--- DZilla/src/components/Brand.jsx
+++ DZilla/src/components/Brand.jsx
@@ -8,16 +8,16 @@ export default function Brand() {
                 <h1>Our Brand Impact</h1>
             </div>
             <div className="brand-card">
-                <img src="src\components\brand-image\0cf6d9bde7e4f37e9725f1762202286fcd81ca49.png" alt="" style={{height:'150px', width:'250px'}} />
-                <img src="src\components\brand-image\3be00125d8a1866374e0c286eb14c9fa1237cc41.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\9a25fbed56202215cf047265b70f53bd1a878222.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\23aa23e724231cfc9679dab2302a4c9e983c0bfc (2).png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\75f7b04c5f580197b6d93b801975da72c0c3caa7.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\303bc0abb27cbe30907d21a7e293d93992ebcf3c (1).png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\50586ffa11d216e5a6c5c65b7fdc69b707ce1500.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\4738647c2b9d3876478b66d3d2f5c8ff4a7a80e2.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\e3e0439489109e94f5e58aa64cde1dd10d2c75b7.png" alt="" style={{height:'150px'}}/>
-                <img src="src\components\brand-image\faff239e7078aa6b120f130ecf78cb44c7062da2.png" alt="" style={{height:'150px'}}/>
+                <img src="src\components\brand-image\0cf6d9bde7e4f37e9725f1762202286fcd81ca49.png" alt="" style={{height:'100px', width:'200px'}} />
+                <img src="src\components\brand-image\3be00125d8a1866374e0c286eb14c9fa1237cc41.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\9a25fbed56202215cf047265b70f53bd1a878222.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\23aa23e724231cfc9679dab2302a4c9e983c0bfc (2).png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\75f7b04c5f580197b6d93b801975da72c0c3caa7.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\303bc0abb27cbe30907d21a7e293d93992ebcf3c (1).png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\50586ffa11d216e5a6c5c65b7fdc69b707ce1500.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\4738647c2b9d3876478b66d3d2f5c8ff4a7a80e2.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\e3e0439489109e94f5e58aa64cde1dd10d2c75b7.png" alt="" style={{height:'100px'}}/>
+                <img src="src\components\brand-image\faff239e7078aa6b120f130ecf78cb44c7062da2.png" alt="" style={{height:'100px'}}/>
             </div>
         </div>
         </>
